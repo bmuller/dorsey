e9258c8161e49f7e634ab8e744a82b60ef1bce3a
Brian Muller <bamuller@gmail.com>
2014-05-04 12:46:26 -0400

    updated routing tests

--- routing_test.go
+++ routing_test.go
@@ -1,18 +1,47 @@
 package dorsey
 
 import (
-	//	. "dorsey"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/op/go-logging"
 )
 
-var _ = Describe("Routing", func() {
-	Describe("A string route", func() {
-		Context("With a named path", func() {
-			It("should make a new path", func() {
-				Expect(1).To(Equal(1))
+var _ = Describe("A server with routing", func() {
+	// shut the logger up
+	logging.SetBackend(logging.NewMemoryBackend(0))
+
+	var (
+		server *Server
+	)
+
+	BeforeEach(func() {
+		server = &Server{Log: logging.MustGetLogger("dorsey")}
+	})
+
+	Context("and a string path", func() {
+		It("should be able to match it", func() {
+			server.Get("/path", func(w *ResponseWriter, r *Request) {
+				w.Render("hi")
 			})
+
+			server.Get("/anotherpath/thats/long", func(w *ResponseWriter, r *Request) {
+				w.Render("anotherhi")
+			})
+
+			w := server.FakeRequest("GET", "/path")
+			Expect(w.Body.String()).To(Equal("hi"))
+
+			w = server.FakeRequest("GET", "/anotherpath/thats/long")
+			Expect(w.Body.String()).To(Equal("anotherhi"))
+		})
+
+		It("should be able to not match a bad path", func() {
+			server.Get("/path/thats/long", func(w *ResponseWriter, r *Request) {
+				w.Render("hi")
+			})
+
+			w := server.FakeRequest("GET", "/path/thats")
+			Expect(w.Code).To(Equal(404))
 		})
 	})
 })
